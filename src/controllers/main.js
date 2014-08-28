dd2140e7fdc9a490f90c176fe6f06879f938b7d1
Sakari Kapanen <sakari.m.kapanen@gmail.com>
2014-08-29 01:19:37 +0300

    Add one more root into default config.

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -14,6 +14,7 @@ fb.controller('MainCtrl', ['$scope', function($scope) {
 
         roots: [
             { root: { Re: -1, Im: 0 }, hue: 0 },
+            { root: { Re:  0, Im: 1 }, hue: 0.167 },
             { root: { Re:  0, Im: 0 }, hue: 0.333 },
             { root: { Re:  1, Im: 0 }, hue: 0.667 }
         ]
